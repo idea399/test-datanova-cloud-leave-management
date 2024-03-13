f8f0c718c66b9f39d2b9ee898119387e496633f0
Senior Coder <idea399@outlook.com>
2024-03-14 05:48:09 +0800

    Fixed deployment issue on vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import fixReactVirtualized from "esbuild-plugin-react-virtualized";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    esbuildOptions: {
-      plugins: [fixReactVirtualized],
-    },
-  },
+  optimizeDeps: {},
 });
